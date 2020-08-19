6fcbb6c51eb00851af0f4cdcf9e8ff88888b1a8f
audunaell <62010741+audunaell@users.noreply.github.com>
2020-08-19 16:07:09 +0200

    added oblig1

---
+++ modul3Oblig1/modul3Oblig1/FamilyApp.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace modul3Oblig1
+{    
+    public class FamilyApp        
+    {
+        public List<Person> FamilyList;
+        public string CommandPrompt = "Input: ";
+
+        public FamilyApp(params Person[] persons)
+        {
+            FamilyList = new List<Person>(persons);
+            
+        }
+               
+        public string HandleCommand(string Command)
+        {
+            string Question = Command;
+                            
+                if (Question.ToLower() == "hjelp")
+                {
+                string HelpMenu = "skriv: 'hjelp' for liste over kommandoer." + "\n" +
+                    "skriv: 'liste' for å se slektninger som er registrert." + "\n" +
+                    "skriv: 'vis <id>' for å se personen med denne id.";                        
+                    return HelpMenu;
+                }
+                else if (Question.ToLower() == "liste")
+                {
+                    string Listing = "";
+                    foreach(var Persons in FamilyList)
+                    {
+                        Listing += Persons.GetDescription() + "\n";
+                    }
+                    return Listing;
+                }
+                else if (Question.Length > 0 && Question.Substring(0, 3).ToLower() == "vis")
+                {
+                    int IdNumber = Int32.Parse(Question.Substring(4));
+                    return FindPerson(IdNumber);                
+                }                
+                else
+                {
+                    string WriteHelp = "skriv: 'hjelp' for liste over kommandoer ";
+                    return WriteHelp;                    
+                }               
+        }
+        public string WelcomeMessage()
+        {
+            string Message = "Velkommen!";
+            return Message;
+        }
+
+        public string FindPerson(int IdNumber)
+        {
+            string InfoToReturn;
+
+            foreach(var SearchedForPersin in FamilyList)
+            {
+                if(SearchedForPersin.Id == IdNumber)
+                {
+                    InfoToReturn = SearchedForPersin.GetDescription();                    
+                    InfoToReturn += FindChildren(IdNumber);
+                    return InfoToReturn;
+                }
+            }
+            InfoToReturn = "Could not find person.";
+            return InfoToReturn;
+        }
+
+        public string FindChildren(int IdNumber)
+        {
+            int maxChildren = FamilyList.Count;
+            string[] IdListOfChildrens = new string[maxChildren];
+            string child = null;
+            int c = 0;
+
+            for (int i = 0; i < maxChildren; i++){
+                
+                if (FamilyList[i].Mother != null && FamilyList[i].Mother.Id == IdNumber)
+                {                    
+                    IdListOfChildrens[c] = "    " + FamilyList[i].FirstName + " (Id=" + FamilyList[i].Id + ") Født: " + FamilyList[i].BirthYear + "\n";
+                    c++;                    
+                }
+                else if (FamilyList[i].Father != null && FamilyList[i].Father.Id == IdNumber)
+                {                    
+                    IdListOfChildrens[c] = "    " + FamilyList[i].FirstName + " (Id=" + FamilyList[i].Id + ") Født: " + FamilyList[i].BirthYear + "\n";
+                    c++;                    
+                }
+            }
+            
+            if(IdListOfChildrens[0] != null)
+            {
+                child = "\n  Barn:\n";
+                for(int i = 0; i < IdListOfChildrens.Length; i++)
+                {                    
+                    if(IdListOfChildrens[i] != null)
+                    {
+                        child += IdListOfChildrens[i];
+                    }                    
+                }
+            }
+            else if (IdListOfChildrens[0] == null)
+            {
+                child = "";
+            }
+
+            
+            return child;
+        }
+    }
+}
---
+++ modul3Oblig1/modul3Oblig1/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace modul3Oblig1
+{
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public int BirthYear;
+        public int DeathYear;
+        public Person Father;
+        public Person Mother;
+        
+                       
+        public string GetDescription()
+        {
+            
+            string InfoFound = CheckIfExist();
+
+            return InfoFound;
+        }
+
+        public string CheckIfExist()
+        {
+            string toReturn = "";
+
+            if(FirstName != null)
+            {
+                toReturn += FirstName + " ";
+            }
+            if (LastName != null)
+            {
+                toReturn += LastName + " ";
+            }
+            if (Id != 0)
+            {
+                toReturn += "(Id=" + Id + ")";
+            }
+            if (BirthYear != 0)
+            {
+                toReturn += " Født: " + BirthYear;
+            }
+            if (DeathYear != 0)
+            {
+                toReturn += " Død: " + DeathYear;
+            }
+            if (Father != null)
+            {
+                toReturn += " Far: " + Father.FirstName + " (Id=" + Father.Id + ")";
+            }
+            if (Mother != null)
+            {
+                toReturn += " Mor: " + Mother.FirstName + " (Id=" + Mother.Id + ")";
+            }
+
+            return toReturn;
+        }        
+    }    
+}
